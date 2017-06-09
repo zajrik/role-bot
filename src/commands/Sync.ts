16a9ba389bb24dd0aaad8a6dd94bf91a6e2a4b98
Zack Campbell <zajrik@gmail.com>
2017-06-09 08:12:48 -0500

    Add command to synchronize desync'd controllers
    In case any roles were added/removed/updated and the bot was offline or having connection issues and failed to receive the related role events.

---
+++ src/commands/Sync.ts
@@ -0,0 +1,30 @@
+import { RoleController } from '../client/RoleController';
+import { RoleClient } from '../client/RoleClient';
+import { Message, TextChannel } from 'discord.js';
+import { Command, Middleware, CommandDecorators } from 'yamdbf';
+const { using } = CommandDecorators;
+
+export default class extends Command<RoleClient>
+{
+	public constructor()
+	{
+		super({
+			name: 'sync',
+			description: 'Resync the controller for an active category',
+			usage: '<prefix>sync <category>',
+			callerPermissions: ['ADMINISTRATOR']
+		});
+	}
+
+	@using(Middleware.expect({ '<category>': 'String' }))
+	public async action(message: Message, [category]: [string]): Promise<any>
+	{
+		await message.delete();
+		const controller: RoleController = this.client.roleManager.getController(message.guild, category);
+		if (!controller) return message.channel
+			.send('Failed to find a role controller for that category.')
+			.then((m: Message) => m.delete(10e3));
+
+		await this.client.roleManager.update(controller.message, category);
+	}
+}
